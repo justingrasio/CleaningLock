e72ae00df8cea856d1ae839586f83d64b71725b7
Grasio Justin <jeremiajustingrasio@gmail.com>
2026-05-13 06:47:28 +0700

    Initial Commit

---
+++ CleaningLock/CleaningLockApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleaningLockApp.swift
+//  CleaningLock
+//
+//  Created by Jeremia Justin Grasio on 13/05/26.
+//
+
+import SwiftUI
+
+@main
+struct CleaningLockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
